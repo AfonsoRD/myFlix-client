a16e945e3e0637ea880d5a2f53969a4254dfee75
Rui Afonso <afonso.r.dev@gmail.com>
2023-02-03 18:31:31 +0100

    Create user update, delete and favorite movies pages

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
 import { Button, Form, Row, Col } from 'react-bootstrap';
-import MovieCard from '../movie-card/movie-card';
 
 const ProfileView = () => {
   return (
