2b927170464efa538fd66143d92ccbf6ea521725
Rui Afonso <afonso.r.dev@gmail.com>
2023-02-07 13:58:02 +0100

    Add Update view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Button, Form, Row, Col } from 'react-bootstrap';
+import { UpdateView } from './update-view';
 
 const ProfileView = () => {
+  const storedToken = localStorage.getItem('token');
+
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+
   return (
-    <Row>
-      <Col>
-        <div>Profile view goes here</div>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col>
+          <div>Profile view goes here</div>
+        </Col>
+      </Row>
+
+      <UpdateView
+        storedToken={storedToken}
+        storedUser={storedUser}
+      />
+    </>
   );
 };
 
